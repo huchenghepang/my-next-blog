d5fe11a36187e4202d8f177914c0d961b7d91987
Jeff <2927678784@qq.com>
2025-02-17 14:41:16 +0800

    新增权限验证

--- src/app/(tooltip)/user/page.tsx
+++ src/app/(tooltip)/user/page.tsx
@@ -1,7 +1,9 @@
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import Button from '@/app/components/Button/Button';
 import IconfontJavaScript from '@/app/components/Iconfont/IconfontJavaScript';
 import LogoutButton from '@/app/components/LoginzButton';
 import { getSession } from '@/app/utils/session';
+import { validatePermission } from '@/app/utils/validatePermission';
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
@@ -15,16 +17,18 @@ export default async function Page() {
   if (!session) {
     redirect("/login");
   }
+
+  const isTogglerRole = await validatePermission(38,58)
   return (
     <div className={userStyle["user-container"]}>
       <IconfontJavaScript scriptName="message_iconfont"></IconfontJavaScript>
       <h2>用户名{session.user?.name}</h2>
       <h2>用户角色{session.user?.currentRole.role_name}</h2>
       <h2>用户ID{session.user?.userId}</h2>
-      <img src={session.user?.image} />
       <p>这个是动态生成的带有类名的组件。</p>
       <Link href={"/loginout"}>退出登录</Link>
       <LogoutButton></LogoutButton>
+      {isTogglerRole && <Button>切换用户角色</Button>}
     </div>
   );
 }
---
+++ src/app/utils/validatePermission.ts
@@ -0,0 +1,60 @@
+import redisClient from '@/db/redis/redis';
+import { Rolepermissions } from "@/types/mysql";
+import { queryByFieldsWithSelectedFields } from './../../db/sql/normal';
+import logger from './logger';
+
+export async function validatePermission(
+    roldId: number,
+    permissionId: number
+) {
+    /* 先在redis中进行权限的验证 如果没有则请求数据库 */
+    try {
+        const RedisKeyName = `role:${roldId}:permission:${permissionId}`;
+        const exists = await redisClient.existsKey(RedisKeyName)
+
+        if (exists.success && exists.data?.exists) {
+            return true
+        }
+        // 检查 Mysql中的权限状态
+        try {
+            /* 需要去mysql数据库获取验证信息 */
+            const result = await checkPermissionsInMySQL(roldId, permissionId);
+            if (result) {
+                /* 存入redis */
+                 
+                redisClient.setKey(RedisKeyName, true);
+                return true
+            } else {
+                return false
+            }
+        } catch (redisError) {
+            const err = redisError as Error
+            if (err.message === "Redis connection timeout or refused") {
+
+                return false
+            }
+            throw err;
+        }
+    } catch (error) {
+        logger.error({error:error as Error,message:"权限验证失败"})
+        return false
+    }
+    /* 在数据库中进行权限的验证 */
+
+}
+
+// 从 MySQL 验证权限
+async function checkPermissionsInMySQL(
+    roleId: number,
+    permissionId: number
+) {
+    try {
+        const result = await queryByFieldsWithSelectedFields<Rolepermissions>("RolePermissions", ["permission_id", "role_id"], [permissionId, roleId], ["permission_id", "role_id"])
+        if (!result ) return null;
+        if(result.data.length === 0) return null
+        return result;
+    } catch (error) {
+        logger.error({ error: error as Error, message: "权限验证失败" })
+        return null
+    }
+}
