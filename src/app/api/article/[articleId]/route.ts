41335c56180c0b9d14d0fc7abe0f89a4612399dd
Jeff <2927678784@qq.com>
2025-02-24 15:45:42 +0800

    完善更新createApiHandler函数

--- src/app/api/article/[articleId]/route.ts
+++ src/app/api/article/[articleId]/route.ts
@@ -1,1 +1,7 @@
-import { createApiHandler } from "@/utils/createApiHandler";
+import { createApiHandler, createApiHandlerWithOptions } from "@/utils/createApiHandler";
+import { sendResponse } from "@/utils/responseHandler/responseHandler";
+
+export const GET = createApiHandlerWithOptions(async (req,{params:{ar}})=>{
+
+    return sendResponse({message:"成功"})
+})
---
+++ src/types/response/api.d.ts
@@ -0,0 +1,1 @@
+export interface Options { params: any };
